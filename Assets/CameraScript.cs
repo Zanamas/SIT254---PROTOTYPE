491a42475ec483747958b5e5b4ef29f1410f032d
Nathan <Nathan_gaming1999@hotmail.com>
2023-09-14 16:33:08 +1000

    code comments
    cleaning up and adding comments to code for easier understanding

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
+
+    /// VARIABLES 
+
+    //added to player vector3 to always be -10units away
     private Vector3 offset = new Vector3(0f, 0f, -10f);
+
+    //how fast/slow the camera repositions to the player
     public float smoothTime = 0.25f;
+
+    //
     Vector3 camVel = Vector3.zero;
 
     public GameObject player;
@@ -16,13 +24,16 @@ public class CameraScript : MonoBehaviour
 
     }
 
-    // Update is called once per frame
+    // FixedUpdate is called once per frame
     void FixedUpdate()
     {
+        // 
         Vector3 targetPosition = player.transform.position + offset;
+
+        //Sets the cameras movement using SmoothDamp to smoothly move it over.
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref camVel, smoothTime);
 
     }
 
-    // comment
+
 }
