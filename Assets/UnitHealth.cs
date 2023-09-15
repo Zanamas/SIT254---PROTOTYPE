25eaa1a06ff615325e47e92527b8e4e7c30c7677
Nathan <40134801+Zanamas@users.noreply.github.com>
2023-09-15 14:42:46 +1000

    bunch of stuff
    created game manager
    fully implimented unithealth
    made the enemy a prefab for future duplication

--- Assets/UnitHealth.cs
+++ Assets/UnitHealth.cs
@@ -17,4 +17,45 @@ public class UnitHealth
     { get { return _currentMaxHealth; } set { _currentMaxHealth = value; } }
 
 
+    //Constructor
+    public UnitHealth(int health, int maxHealth)
+    {
+        _currentHealth = health;
+        _currentMaxHealth = maxHealth;
+    } 
+
+
+    //Methods
+
+    // Run to damage the unit
+    public void DmgUnit(int dmgAmount)
+    {
+        if (_currentHealth > 0)
+        {
+            _currentHealth -= dmgAmount;
+        }
+    }
+
+
+    //run to heal the unit
+    public void healUnit(int healAmount)
+    {
+        //checks if unit can be healed
+        if (_currentHealth < _currentMaxHealth)
+        {
+
+            //prevents overheal if applicable
+            if (_currentHealth + healAmount > _currentHealth)
+            {
+                _currentHealth = _currentMaxHealth;
+            }
+
+            //if cannot overheal
+            else
+            {
+                _currentHealth += healAmount;
+            }
+            
+        }
+    }
 }
