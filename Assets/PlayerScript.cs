8860cd5ca1e96d0090d2de9c8560d77ed0091543
Nathan <Nathan_gaming1999@hotmail.com>
2023-09-18 14:38:41 +1000

    lots of stufff

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -11,17 +11,20 @@ public class PlayerScript : MonoBehaviour
     public Rigidbody2D pRigid;
     public SpriteRenderer pSprite;
     public BoxCollider2D pGround;
+    public CircleCollider2D pCircle;
     public bool isGrounded = false;
     public int speed;
     public int jumpPower;
     Vector2 pMove;
     public UnitHealth pHealth;
+    public int knockBackStr;
 
     // Start is called before the first frame update
     void Start()
     {
-        pHealth = new UnitHealth(6, 6);   
-        
+        pHealth = new UnitHealth(6, 6);
+        Debug.Log("start"); 
+
     }
 
     // Fixed update is used for the player character, this code is run every frame of the game.
@@ -54,10 +57,21 @@ public class PlayerScript : MonoBehaviour
             playerUp.y = Input.GetAxisRaw("Jump") * jumpPower;
             pRigid.velocity = playerUp;
         }
+    
 
     }
 
+    //when the player touches an enemy and gets damaged
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Vector3 knockBack = new Vector3(transform.position.x - collision.gameObject.transform.position.x, transform.position.y - collision.gameObject.transform.position.y, 0);
+            pRigid.velocity = (knockBack * knockBackStr);
+        }
 
+
+    }
     // code that uses the box collider on the player character to see if they are grounded
     // if on the ground, they can jump!
     public void OnTriggerEnter2D(Collider2D collision)
